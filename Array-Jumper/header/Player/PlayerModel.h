46836075d45b10fb0a43c2334ab850364a555289
Tarun <tarunkumarsharma38@gmail.com>
2024-12-12 19:12:05 +0530

    added more logic in the PlayerModel sole

--- Array-Jumper/header/Player/PlayerModel.h
+++ Array-Jumper/header/Player/PlayerModel.h
@@ -1,1 +1,18 @@
 #pragma once
+
+namespace Player {
+	class PlayerModel {
+	private:
+		float m_currentPosition;
+
+	public:
+
+		void SetPlayerPosition(float _positionChange);
+		float GetPlayer();
+
+		enum class PlayerState {
+			Alive,
+			Dead,
+		};
+	};
+}
